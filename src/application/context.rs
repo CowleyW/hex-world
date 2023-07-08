ada9340391f64ed4ea5a7673662dde7f0d9a2299
William Cowley <willcowley99@gmail.com>
2023-07-08 15:50:27 -0400

    Added depth buffer

--- src/application/context.rs
+++ src/application/context.rs
@@ -69,7 +69,8 @@ pub struct Context {
     camera_bind_group: wgpu::BindGroup,
     camera_controller: CameraController,
     instances: Vec<Instance>,
-    instance_buffer: wgpu::Buffer
+    instance_buffer: wgpu::Buffer,
+    depth_texture: Texture
 }
 
 impl Context {
@@ -228,6 +229,8 @@ impl Context {
             label: Some("camera_bind_group_layout")
         });
 
+        let depth_texture = Texture::create_depth_texture(&device, &config, "depth_texture");
+
         let instances = (0..10).flat_map(|z| {
             (0..10).map(move |x| {
                 let position = cgmath::Vector3 { x: (x + 1) as f32, y: 0.0, z: (z + 1) as f32 };
@@ -270,7 +273,13 @@ impl Context {
                 })],
             }),
             primitive: wgpu::PrimitiveState::default(),
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: wgpu::TextureFormat::Depth32Float,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState::default(),
             multiview: None,
         });
@@ -293,7 +302,8 @@ impl Context {
             camera_bind_group,
             camera_controller,
             instances,
-            instance_buffer
+            instance_buffer,
+            depth_texture
         }
     }
 
@@ -303,6 +313,7 @@ impl Context {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.depth_texture = Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -333,7 +344,14 @@ impl Context {
                     store: true,
                 },
             })],
-            depth_stencil_attachment: None,
+            depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                view: &self.depth_texture.view,
+                depth_ops: Some(wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(1.0),
+                    store: true,
+                }),
+                stencil_ops: None,
+            }),
         });
 
         render_pass.set_pipeline(&self.pipeline);
